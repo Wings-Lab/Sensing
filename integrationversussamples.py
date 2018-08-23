2a00b255881c46d005904af93e1866621134a5f1
ysqyang <ysqyang@gmail.com>
2018-08-23 15:06:11 -0400

    adding data_odroid

--- integrationversussamples.py
+++ integrationversussamples.py
@@ -3,6 +3,6 @@ import numpy as np
 
 k = 1
 for i in range(10):
-    for j in range(20):
-        os.system('./rtl_power_fftw -b 8 -e 2 -f 915.8e6 -q -g 1 -t '+str(k)+'s | grep -c \'#\' >>data/'+str(i)+'.txt')
+    for j in range(50):
+        os.system('./rtl_power_fftw -b 8 -e 2 -f 915.8e6 -q -g 1 -t '+str(k)+'s | grep -c \'#\' >>data_odroid/'+str(i)+'.txt')
     k = k/10.0
